a497cafa2c4438f07988071fca7acbc75a8b07a0
Sadiya Fatima Khwaja <104360019+Sadiya-27@users.noreply.github.com>
2025-06-05 00:03:51 +0530

    Update env.ts

--- src/env.ts
+++ src/env.ts
@@ -1,27 +1,6 @@
 import { createEnv } from "@t3-oss/env-nextjs";
 import { z } from "zod";
 
-console.log("Checking env vars:");
-console.log({
-  POSTGRES_URL: process.env.POSTGRES_URL,
-  POSTGRES_PRISMA_URL: process.env.POSTGRES_PRISMA_URL,
-  POSTGRES_URL_NO_SSL: process.env.POSTGRES_URL_NO_SSL,
-  POSTGRES_URL_NON_POOLING: process.env.POSTGRES_URL_NON_POOLING,
-  POSTGRES_USER: process.env.POSTGRES_USER,
-  POSTGRES_HOST: process.env.POSTGRES_HOST,
-  POSTGRES_PASSWORD: process.env.POSTGRES_PASSWORD,
-  POSTGRES_DATABASE: process.env.POSTGRES_DATABASE,
-  CLERK_SECRET_KEY: process.env.CLERK_SECRET_KEY,
-  BLOB_READ_WRITE_TOKEN: process.env.BLOB_READ_WRITE_TOKEN,
-  OPENAI_API_KEY: process.env.OPENAI_API_KEY,
-  STRIPE_SECRET_KEY: process.env.STRIPE_SECRET_KEY,
-  STRIPE_WEBHOOK_SECRET: process.env.STRIPE_WEBHOOK_SECRET,
-  NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY,
-  NEXT_PUBLIC_BASE_URL: process.env.NEXT_PUBLIC_BASE_URL,
-});
-
-
-
 export const env = createEnv({
   server: {
     POSTGRES_URL: z.string().min(1),
