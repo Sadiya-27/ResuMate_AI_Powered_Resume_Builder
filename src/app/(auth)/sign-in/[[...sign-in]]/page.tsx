07237d9ebd4575077daf504e975ec0794a679145
Sadiya Fatima Khwaja <104360019+Sadiya-27@users.noreply.github.com>
2025-06-04 23:37:48 +0530

    Update page.tsx

--- src/app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ src/app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -2,7 +2,7 @@ import { SignIn } from "@clerk/nextjs";
 
 export default function Page() {
   return (
-    <main className="flex h-screen items-center justify-center p-3">
+    <main className="flex h-screen bg-blue-200 items-center justify-center p-3">
       <SignIn />
     </main>
   );
